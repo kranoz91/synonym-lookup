d2879c5d76583c57a6f7c021b8a7c6aef0e2ada4
kranoz91 <martineriksson91@hotmail.se>
2023-09-15 21:02:10 +0200

    Search Bar CSS

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { Grid, ThemeProvider, createTheme } from '@mui/material';
 import { Container } from '@mui/material';
 import { useState } from 'react';
 import { SearchBar } from './components/search/SearchBar';
-
-
+import { Paper } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   const [searchString, setSearchString] = useState('');
@@ -21,11 +23,45 @@ function App() {
     setOnClickSearchString(searchString);
   }
 
+  const defaultTheme = createTheme();
+
   return (
-    <Container>
-      <SearchBar HandleChange={handleChange} HandleSearch={handleSearch}/>
-      <Typography>{onClickSearchString}</Typography>
-    </Container>
+    <ThemeProvider theme={defaultTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+            alignContent: 'center'
+          }}
+        >
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Grid container spacing={3} sx={{ flexDirection: 'column', alignContent: 'center' }}>
+              <Grid item xs={12} md={8} lg={9}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 240,
+                  }}
+                >
+                  <SearchBar HandleChange={handleChange} HandleSearch={handleSearch}/>
+                  <Typography>{onClickSearchString}</Typography>
+                </Paper>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
 
